081df2a6d6b17f8a40a029d4ef77b7302d360b6e
ibnuhaji66 <ibnuhaji66@gmail.com>
2024-08-13 12:09:59 +0700

    Add files via upload

---
+++ selection-ternary.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	
+	//SELECTION
+	// 1. Ternary
+	// Kondisi ? benar : salah
+	int angka = 1;
+	printf("%s", (angka < 2? "lebih kecil dari 2" : angka > 2? "lebih besar dari 2" : "2"));
+}
+

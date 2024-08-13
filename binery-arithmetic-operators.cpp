b49f61added3845e8a1de37f4133ac548bfc5af9
ibnuhaji66 <ibnuhaji66@gmail.com>
2024-08-13 12:04:13 +0700

    Add files via upload

---
+++ binery-arithmetic-operators.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+	
+	printf("Ini tambah : %d\n", 2+3);
+	printf("Ini kurang : %d\n", 3-2);
+	printf("Ini kali : %d\n", 3*3);
+	printf("Ini bagi : %d\n", 5/2);
+	printf("Ini bagi (desimal) : %f\n", 5.0/2.0);
+	printf("Ini Arithmetic : (%d+%d)/%d = %d\n", 5,2,3, (5+2)/3);
+	
+	return 0;
+	
+}
+
